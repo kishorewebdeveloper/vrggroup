be451b95e72032822b3aa0b994ec43dcede1bf52
kishorewebdeveloper <kishorewebdeveloper@gmail.com>
2022-11-22 01:01:15 +0530

    Carousel added

---
+++ Carousel/js/main.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	
+    $('.owl-1').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'slideInUp',
+        items:1,
+        autoplay: true,
+        loop: true,
+        nav:true,
+        margin:0,
+        stagePadding:0,
+        smartSpeed:250,
+        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+    });
+
+    $('.owl-2').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'slideInUp',
+        items:1,
+        autoplay: true,
+        loop: true,
+        nav:true,
+        margin:0,
+        stagePadding:0,
+        smartSpeed:350,
+        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+    });
+
+    
+    $('.owl-3').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'slideInUp',
+        items:1,
+        autoplay: true,
+        loop: true,
+        nav:true,
+        margin:0,
+        stagePadding:0,
+        smartSpeed:350,
+        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+    });
+    
+
+	
+})
